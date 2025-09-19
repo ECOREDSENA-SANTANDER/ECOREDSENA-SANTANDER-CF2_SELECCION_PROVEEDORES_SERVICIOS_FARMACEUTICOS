42364e7d272e9804770f4ad37a82f19ecc53fdff
Leonardo Castellanos R. <160909833+IngLeoCastellanos@users.noreply.github.com>
2025-09-19 14:28:33 -0500

    Initial commit

---
+++ src/config/global.js
@@ -0,0 +1,191 @@
+export default {
+  global: {
+    Name: 'Nombre del recurso educativo',
+    Description: 'Descripción del RED',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
+    fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
+    imagenesDecorativasBanner: [
+      {
+        clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
+      },
+      {
+        clases: ['banner-principal-decorativo-2'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+      },
+    ],
+  },
+  menuPrincipal: {
+    menu: [
+      {
+        nombreRuta: 'inicio',
+        icono: 'fas fa-home',
+        titulo: 'Volver al inicio',
+      },
+      {
+        nombreRuta: 'introduccion',
+        icono: 'fas fa-info-circle',
+        titulo: 'Introducción',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema1',
+        numero: '1',
+        titulo: 'Tema 1',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '1.1',
+            titulo: 'Subtema 1',
+            hash: 't_1_1',
+          },
+        ],
+      },
+
+      {
+        nombreRuta: 'tema2',
+        numero: '2',
+        titulo: 'Tema 2',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema3',
+        numero: '3',
+        titulo: 'Tema 3',
+        desarrolloContenidos: true,
+      },
+    ],
+    subMenu: [
+      {
+        icono: 'fas fa-sitemap',
+        titulo: 'Síntesis',
+        nombreRuta: 'sintesis',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'actividad',
+        icono: 'far fa-question-circle',
+        titulo: 'Actividad didáctica',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'glosario',
+        icono: 'fas fa-sort-alpha-down',
+        titulo: 'Glosario',
+      },
+      {
+        nombreRuta: 'complementario',
+        icono: 'far fa-folder-open',
+        titulo: 'Material complementario',
+      },
+      {
+        icono: 'fas fa-book',
+        titulo: 'Referencias bibliográficas',
+        nombreRuta: 'referencias',
+      },
+      {
+        icono: 'fas fa-file-pdf',
+        titulo: 'Descargar PDF',
+        download: 'downloads/dist.pdf',
+      },
+      {
+        icono: 'fas fa-download',
+        titulo: 'Descargar material',
+        download: 'downloads/material.zip',
+      },
+      {
+        icono: 'far fa-registered',
+        titulo: 'Créditos',
+        nombreRuta: 'creditos',
+      },
+    ],
+  },
+  complementario: [
+    {
+      tema: 'Tema 1',
+      referencia: 'Texto',
+      tipo: 'Sitio web',
+      link: 'Link',
+    },
+  ],
+  glosario: [
+    {
+      termino: 'Término',
+      significado: 'Definición',
+    },
+  ],
+  referencias: [
+    {
+      referencia: '',
+      link: '',
+    },
+  ],
+  creditos: [
+    {
+      titulo: 'ECOSISTEMA DE RECURSOS EDUCATIVOS DIGITALES',
+      autores: [
+        {
+          nombre: 'Milady Tatiana Villamil Castellanos',
+          cargo: 'Responsable del ecosistema',
+          centro: 'Dirección General',
+        },
+        {
+          nombre: 'Claudia Johanna Gómez Pérez',
+          cargo: 'Responsable de línea de producción',
+          centro: 'Centro Agroturístico - Regional Santander',
+        },
+      ],
+    },
+    {
+      titulo: 'CONTENIDO INSTRUCCIONAL',
+      autores: [
+        {
+          nombre: ' ',
+          cargo: ' ',
+          centro: 'Centro XYZ - Regional XYZ',
+        },
+      ],
+    },
+    {
+      titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
+      autores: [
+        {
+          nombre: ' ',
+          cargo: 'Diseñador de contenidos',
+          centro: 'Centro Agroturístico - Regional Santander',
+        },
+        {
+          nombre: ' ',
+          cargo: 'Desarrollador <i>full stack</i>',
+          centro: 'Centro Agroturístico - Regional Santander',
+        },
+        {
+          nombre: ' ',
+          cargo: 'Animador y productor audiovisual',
+          centro: 'Centro Agroturístico - Regional Santander',
+        },
+      ],
+    },
+    {
+      titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
+      autores: [
+        {
+          nombre: ' ',
+          cargo: 'Validador y vinculador de recursos educativos digitales',
+          centro: 'Centro Agroturístico - Regional Santander',
+        },
+        {
+          nombre: ' ',
+          cargo: 'Evaluador de contenidos inclusivos y accesibles',
+          centro: 'Centro Agroturístico - Regional Santander',
+        },
+      ],
+    },
+  ],
+  creditosAdicionales: {
+    imagenes:
+      'Fotografías y vectores tomados de <a href="https://www.freepik.es/" target="_blank">www.freepik.es</a>, <a href="https://www.shutterstock.com/" target="_blank">www.shutterstock.com</a>, <a href="https://unsplash.com/" target="_blank">unsplash.com </a>y <a href="https://www.flaticon.com/" target="_blank">www.flaticon.com</a>',
+    creativeCommons:
+      'Licencia creative commons CC BY-NC-SA<br><a href="https://creativecommons.org/licenses/by-nc-sa/2.0/" target="_blank">ver licencia</a>',
+  },
+}
---
+++ src/views/sintesis.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. 
+    
+
+    .row.justify-content-center
+      .col-lg-10.mb-5.bgs.p-4.brad
+        figure
+          img(src="@/assets/curso/sintesis.svg", alt="Descripción imagen")
+      .col-auto
+        a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
+          .anexo__icono
+            img(src="@/assets/bullets/icono-pdf.svg")
+          .anexo__texto
+            p Anexo. Síntesis
+
+</template>
+
+<script>
+export default {
+  name: 'Sintesis',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
